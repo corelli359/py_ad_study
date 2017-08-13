ff6aa9c4052e65e6afbe032991e1876c534a4701
corelli359 <179883253@qq.com>
2017-08-14 00:21:52 +0800

    update

--- study/st_1.py
+++ study/st_1.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from types import MappingProxyType
-from requests import Session
+import random
+import time
 
 
-class MappingClassTest(dict):
+# 私有属性_dic 不被外部控制，但是可以随着初始化时的引入变量的值的改变而改变
+class MappingClassTest:
     def __init__(self, dic):
         if not isinstance(dic, dict):
             raise TypeError(dic)
@@ -13,10 +14,47 @@ class MappingClassTest(dict):
         return str(self._dic)
 
 
+def effect_set_list():
+    def choice_num():
+        return random.sample(range(10000000), 1000)
+
+    def func_1():
+        start = time.time()
+        count = 0
+        for n in choice_list:
+            if n in total:
+                count += 1
+        if count == 1000:
+            print('func_1 takes {} secs'.format(str(time.time() - start)))
+
+    def func_2():
+        start = time.time()
+        count = len(set(choice_list) & set(total))
+        if count == 1000:
+            print('func_2 takes {} secs'.format(str(time.time() - start)))
+
+    choice_list = choice_num()
+    total = [x for x in range(10000000)]
+    func_2()
+    func_1()
+
+
+# 利用此类进行__call__的理解,利用__call__将对象函数化，可被调用
+class BingoCage:
+    def __init__(self, items):
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+
+    def __call__(self):
+        return self.pick()
+
+
 if __name__ == '__main__':
-    d = {'name': 'hello'}
-    mc = MappingClassTest(d)
-    mp = MappingProxyType(d)
-    print(mc)
-    print(type(mc))
-    print(type(mp))
+    bg = BingoCage(range(5))
+    print(callable(bg))
